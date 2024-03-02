139f5b537ca2783351040754c1de7bfb29c230b3
Bonex1s <bonex1@ukr.net>
2024-03-02 22:17:14 +0200

    commands

--- botLauncher.mjs
+++ botLauncher.mjs
@@ -1,32 +1,9 @@
 import { REST, Routes } from "discord.js";
 import { config } from "./config.mjs";
+import { commands } from "./commands.mjs";
 
 const clientId = "1204613780554911814";
 const token = config.token;
-
-const commands = [
-  {
-    name: "random",
-    description: "Random number!",
-  },
-  { name: "teemo", description: "bulling!" },
-  {
-    name: "position",
-    description: "Random Lol positon!",
-  },
-  {
-    name: "winrate",
-    description: "KH Bonex winrate",
-  },
-  {
-    name: "statsyordle",
-    description: "HappyYordle stats win/lose",
-  },
-  {
-    name: "statsbonex",
-    description: "Bonex stats win/lose",
-  },
-];
 
 const rest = new REST({ version: "10" }).setToken(token);
 
---
+++ commands.mjs
@@ -0,0 +1,25 @@
+const commands = [
+  {
+    name: "random",
+    description: "Random number!",
+  },
+  { name: "teemo", description: "bulling!" },
+  {
+    name: "position",
+    description: "Random Lol positon!",
+  },
+  {
+    name: "winrate",
+    description: "KH Bonex winrate",
+  },
+  {
+    name: "statsyordle",
+    description: "HappyYordle stats win/lose",
+  },
+  {
+    name: "statsbonex",
+    description: "Bonex stats win/lose",
+  },
+];
+
+export { commands };
